4777b475baf0b8dd195631e534d957898c37508d
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-11-22 16:04:28 +0200

    feat: add New Student button (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { Box, Button, Stack } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import { StudentsTable } from "./components/StudentsTable";
+
 function App() {
   return (
     <>
       <h1>Students table</h1>
-      <StudentsTable />
+      <Box sx={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+        <Stack direction={"row"} justifyContent={"flex-end"}>
+          <Button variant="contained" startIcon={<AddIcon />}>
+            Add Student
+          </Button>
+        </Stack>
+        <StudentsTable />
+      </Box>
     </>
   );
 }
